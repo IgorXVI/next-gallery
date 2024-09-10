4ea202e32fbbed4ab709f65e5b653040b3d47176
Igor Tomelero de Almeida <inazumaseleven04@gmail.com>
2024-09-10 00:35:28 -0300

    setting up sentry

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default async function HomePage() {
             >
               <Image
                 src={img.url}
-                alt="image"
+                alt={img.name}
                 width={500}
                 height={500}
               />
