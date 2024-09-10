a64bd6c3750af086b4ac8f3bf79552e28c4561ef
Igor Tomelero de Almeida <inazumaseleven04@gmail.com>
2024-09-09 22:44:37 -0300

    use auth for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image"
+import { SignedIn, SignedOut } from "@clerk/nextjs"
+
 import { db } from "../server/db"
 
 export const dynamic = "force-dynamic"
@@ -13,22 +15,29 @@ export default async function HomePage() {
       className="flex min-h-screen flex-col 
       items-center text-white"
     >
-      <div className="flex flex-wrap gap-4 p-5">
-        {images.map((img) => (
-          <div
-            className="w-48 flex flex-col items-center"
-            key={img.id}
-          >
-            <Image
-              src={img.url}
-              alt="image"
-              width={500}
-              height={500}
-            />
-            <p>{img.name}</p>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <p className="h-full w-full text-2xl text-center font-extrabold text-red-500">
+          Please sign in above
+        </p>
+      </SignedOut>
+      <SignedIn>
+        <div className="flex flex-wrap gap-4 p-5">
+          {images.map((img) => (
+            <div
+              className="w-48 flex flex-col items-center"
+              key={img.id}
+            >
+              <Image
+                src={img.url}
+                alt="image"
+                width={500}
+                height={500}
+              />
+              <p>{img.name}</p>
+            </div>
+          ))}
+        </div>
+      </SignedIn>
     </main>
   )
 }
